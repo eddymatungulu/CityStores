e62e490ba40dd591d0c7ec671e3e490394949b83
eddymatcisco <eddymatcisco@gmail.com>
2019-01-23 21:49:14 +0200

    implementing response wrapper call

--- citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
+++ citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
@@ -1,10 +1,9 @@
 package com.goddy.citystoreslibrary.data.api
 
-import android.arch.lifecycle.LiveData
-import com.goddy.citystoreslibrary.models.City
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService{
     @GET("/v2/5b7e8bc03000005c0084c210")
-    fun fetchRemoteData():LiveData<ApiResponse<List<City>>>
+    fun fetchRemoteData():Call<WrapperResponse>
 }
