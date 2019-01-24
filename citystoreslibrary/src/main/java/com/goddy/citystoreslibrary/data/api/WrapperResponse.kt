bd86d6431d32266a7e13f647f65f6d070e009e31
eddymatcisco <eddymatcisco@gmail.com>
2019-01-24 02:53:56 +0200

    wrapper as list of city apiResponse

--- citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/WrapperResponse.kt
+++ citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/WrapperResponse.kt
@@ -1,10 +1,9 @@
 package com.goddy.citystoreslibrary.data.api
 
-import android.arch.lifecycle.LiveData
 import com.goddy.citystoreslibrary.models.City
 import com.google.gson.annotations.SerializedName
 
 data class WrapperResponse(
         @SerializedName("cities")
-        val items: List<City>
+        val items: ApiResponse<List<City>>
 )
