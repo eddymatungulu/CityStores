77cdc0b058375b33afc4e04b37c1ec5d85cdabee
eddymatungulu <eddy@2waysystems.com>
2019-01-24 13:04:23 +0200

    back to List permission plus GSON

--- citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
+++ citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
@@ -1,12 +1,10 @@
 package com.goddy.citystoreslibrary.data.api
 
-import android.arch.lifecycle.LiveData
-import com.goddy.citystoreslibrary.models.City
-import org.json.JSONObject
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService{
     @GET("/v2/5b7e8bc03000005c0084c210")
-    fun fetchRemoteData():Call<JSONObject>
+    fun fetchRemoteData():Call<JsonObject>
 }
