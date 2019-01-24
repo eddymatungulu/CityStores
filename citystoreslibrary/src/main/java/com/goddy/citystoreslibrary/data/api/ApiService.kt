cb498015c41e1cbffa887e95f5946f29646c991b
eddymatungulu <eddy@2waysystems.com>
2019-01-24 09:08:37 +0200

    back to List

--- citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
+++ citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
@@ -1,9 +1,10 @@
 package com.goddy.citystoreslibrary.data.api
 
 import android.arch.lifecycle.LiveData
+import com.goddy.citystoreslibrary.models.City
 import retrofit2.http.GET
 
 interface ApiService{
     @GET("/v2/5b7e8bc03000005c0084c210")
-    fun fetchRemoteData():LiveData<ApiResponse<WrapperResponse>>
+    fun fetchRemoteData():LiveData<ApiResponse<List<City>>>
 }
