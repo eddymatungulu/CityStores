a7dfc6f30863eef74e6f6ecc984b7158c5ba0243
eddymatcisco <eddymatcisco@gmail.com>
2019-01-26 15:11:11 +0200

    wrapper as list of city apiResponse WrapperResponse

--- citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
+++ citystoreslibrary/src/main/java/com/goddy/citystoreslibrary/data/api/ApiService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 
 interface ApiService{
     @GET("/v2/5b7e8bc03000005c0084c210")
-    fun fetchRemoteData():Call<JSONObject>
+    fun fetchRemoteData():Call<WrapperResponse>
 }
